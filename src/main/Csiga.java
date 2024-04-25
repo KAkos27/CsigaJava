83f857dc9db0a701de7d856403b56a96ffba9421
KAkos27 <akoskosztolanyi@gmail.com>
2024-04-25 10:42:14 +0200

    Csiga

--- src/main/Csiga.java
+++ src/main/Csiga.java
@@ -1,34 +1,35 @@
 package main;
 
+import java.util.Random;
+
 public class Csiga {
 
     private String szin;
     private int lepes;
     private int megtett;
 
-    public void setSzin() {
-
+    public void setSzin(String nev, String szinkod) {
+        szin = szinkod + nev;
     }
 
-    ;
     public String getSzin() {
-        return "";
+        return szin;
     }
 
-    ;
     public void setLepes() {
-
+        Random rnd = new Random();
+        lepes = rnd.nextInt(3);
     }
 
     public int getLepes() {
-        return 1;
+        return lepes;
     }
 
-    public void setMegtett() {
-
+    public void setMegtett(int lepes) {
+        megtett += lepes;
     }
 
     public int getMegtett() {
-        return 1;
+        return megtett;
     }
 }
