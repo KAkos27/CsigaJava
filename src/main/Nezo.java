fc569d95b5eecc7697725626b8c73af7b6d4d639
lajkogege <lajkogege@gmail.com>
2024-04-25 10:23:08 +0200

    nezo.java

---
+++ src/main/Nezo.java
@@ -0,0 +1,14 @@
+
+package main;
+
+public class Nezo {
+    private static String fogadas;
+
+    public int setFogadas(){
+        return 1;
+    }
+    
+    public String getFogadas(){
+        return "";
+    }
+}
